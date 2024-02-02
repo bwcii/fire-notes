3637e055a141e4b43d5adeebad5158566cada520
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-02-02 05:07:02 +0000

    initial setup for firebase hosting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"fire-notes-99c74","appId":"1:176427324007:web:5e597a269a9d4a26e23ac2","storageBucket":"fire-notes-99c74.appspot.com","apiKey":"AIzaSyC7_f-h90XELp-nKo8dyVs84lHU6Pb7314","authDomain":"fire-notes-99c74.firebaseapp.com","messagingSenderId":"176427324007","measurementId":"G-830EVYMVW7"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
